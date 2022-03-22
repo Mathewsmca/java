03845567ec880026a54d1cea277dac8455320333
Mathewsmca <98259029+Mathewsmca@users.noreply.github.com>
2022-03-22 21:55:05 +0530

    Add files via upload

---
+++ Append.java
@@ -0,0 +1,8 @@
+public  class Append{
+ public static void main(String args[]){
+  String s1="new";
+  String s2=" world";
+  String a=s1+s2;
+  System.out.println(a);
+  }  
+ }
---
+++ Operations.java
@@ -0,0 +1,17 @@
+public class Operations
+{
+ public static void main(String args[]){
+ int a=100;
+ int b=15;
+ int sum=a+b;
+ int mul=a*b;
+ int div=a%b;
+ int sub=a-b;
+ System.out.println("Addition :"+sum);
+ System.out.println("Multiplication: "+mul);
+ System.out.println("Division: "+div);
+ System.out.println("Substraction: "+sub);
+ }
+ }
+ 
+  
